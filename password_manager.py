d19763c1ee7b95ed65957d331094c52b7a00ad90
Jenna Perez <jmsp43@gmail.com>
2023-07-15 13:25:55 -0400

    view and add funcs added

--- password_manager.py
+++ password_manager.py
@@ -2,7 +2,11 @@ master_pwd = input("What is the master password? ")
 
 
 def view():
-  pass
+    #r means read file without making any changes
+    with open('passwords.txt', 'r') as file:
+      for line in file.readlines():
+            print(line)
+
 
 def add():
   name = input('Account Name: ')
@@ -11,7 +15,7 @@ def add():
   #using with will automatically close file for you after using it
   #a means append to file end without changing anything else in it, and if it doesn't already exists it will create a new file
   with open('passwords.txt', 'a') as file:
-    file.write(name + '|' + pwd)
+    file.write(name + '|' + pwd + "\n")
 
 
     
